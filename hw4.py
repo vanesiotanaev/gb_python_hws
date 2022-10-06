6d718aae926afee9794b5c4b316dd07b2ddd8ee8
Ivan <vanesiotanaev@mail.ru>
2022-10-06 15:16:37 +0300

    hw4 ex3 completed

--- hw4.py
+++ hw4.py
@@ -45,31 +45,57 @@
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-def prime_factors(N):
-    while N <= 0:
-        N = int(input(f'"{N}" не является натуральным числом! Введите натуральное число: ')) 
-    while N == 1:
-        N = int(input(f'"1" нельзя разложить на простые множители, т.к. у него всего один делитель! Введите натуральное число != 1: '))
-    list1 = []
-    for i in range(2, N+1):
-        list1.append(i)
-    list2 = []
-    for item in list1:
-        count = 0
-        for i in range(1, item+1):
-            if item % i == 0:
-                count += 1
-        if count == 2:
-            list2.append(item)
-    prime_factors = frozenset(list2)
-    list3 = []
-    for i in range(2, N+1):
-        if i in prime_factors and (N % i == 0):
-            list3.append(i)
+# def prime_factors(N):
+#     while N <= 0:
+#         N = int(input(f'"{N}" не является натуральным числом! Введите натуральное число: ')) 
+#     while N == 1:
+#         N = int(input(f'"1" нельзя разложить на простые множители, т.к. у него всего один делитель! Введите натуральное число != 1: '))
+#     list1 = []
+#     for i in range(2, N+1):
+#         list1.append(i)
+#     list2 = []
+#     for item in list1:
+#         count = 0
+#         for i in range(1, item+1):
+#             if item % i == 0:
+#                 count += 1
+#         if count == 2:
+#             list2.append(item)
+#     prime_factors = frozenset(list2)
+#     list3 = []
+#     for i in range(2, N+1):
+#         if i in prime_factors and (N % i == 0):
+#             list3.append(i)
             
-    return list3
+#     return list3
 
 
 
-user_value = int(input("Введите натуральное число, которое нужно разложить на простые множители: "))
-print(f'У числа {user_value} есть простые множители: {prime_factors(user_value)}')
+# user_value = int(input("Введите натуральное число, которое нужно разложить на простые множители: "))
+# print(f'У числа {user_value} есть простые множители: {prime_factors(user_value)}')
+
+# 3. Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
+# исходной последовательности.
+# Пример:
+# 47756688399943 -> [5]
+# 1113384455229 -> [8,9]
+# 1115566773322 -> []
+
+def unique_elements_sequence(sequence):
+    list_sequence = list(str(sequence))
+    unique_sequence_list = list(set(list_sequence))
+    final_unique_list = []
+    for item in unique_sequence_list:
+        count = 0
+        for element in list_sequence:
+            if element == item:
+                count += 1
+        if count == 1:
+            final_unique_list.append(int(item))
+    final_unique_list.sort()
+
+    return final_unique_list
+
+
+user_sequence = 1113384455229
+print(f"Неповторяющиеся цифры в последовательности [{user_sequence}]: {unique_elements_sequence(user_sequence)}")
