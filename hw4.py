7373b54304b6de86df0ee300260b0a19ef289f59
Ivan <vanesiotanaev@mail.ru>
2022-10-06 16:43:52 +0300

    hw4 ex4 completed

--- hw4.py
+++ hw4.py
@@ -81,21 +81,56 @@
 # 1113384455229 -> [8,9]
 # 1115566773322 -> []
 
-def unique_elements_sequence(sequence):
-    list_sequence = list(str(sequence))
-    unique_sequence_list = list(set(list_sequence))
-    final_unique_list = []
-    for item in unique_sequence_list:
-        count = 0
-        for element in list_sequence:
-            if element == item:
-                count += 1
-        if count == 1:
-            final_unique_list.append(int(item))
-    final_unique_list.sort()
+# def unique_elements_sequence(sequence):
+#     list_sequence = list(str(sequence))
+#     unique_sequence_list = list(set(list_sequence))
+#     final_unique_list = []
+#     for item in unique_sequence_list:
+#         count = 0
+#         for element in list_sequence:
+#             if element == item:
+#                 count += 1
+#         if count == 1:
+#             final_unique_list.append(int(item))
+#     final_unique_list.sort()
 
-    return final_unique_list
+#     return final_unique_list
 
 
-user_sequence = 1113384455229
-print(f"Неповторяющиеся цифры в последовательности [{user_sequence}]: {unique_elements_sequence(user_sequence)}")
+# user_sequence = 1113384455229
+# print(f"Неповторяющиеся цифры в последовательности [{user_sequence}]: {unique_elements_sequence(user_sequence)}")
+
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от -100 до 100)
+# многочлена и записать в файл многочлен степени k
+# k - максимальная степень многочлена, следующий степень следующего на 1 меньше и так до ноля
+# Коэффициенты расставляет random, поэтому при коэффициенте 0 просто пропускаем данную итерацию степени
+import random
+
+def generate_equation(k):
+    power_keys = []
+    for i in range(k, -1, -1):
+        power_keys.append(i)
+    
+    equation = ''
+    dictionary = dict.fromkeys(power_keys)
+    plus_minus = ['-', '+']
+    for key in dictionary:
+        dictionary[key] = random.randint(0, 5)
+        if key == 0:
+            equation += f' {str(dictionary[key])} {random.choice(plus_minus)}'
+        elif key == 1:
+            equation += f' {str(dictionary[key])}x {random.choice(plus_minus)}'
+        else:
+            if dictionary[key] != 0:
+                if dictionary[key] == 1:
+                    equation += f' x**{key} {random.choice(plus_minus)}'
+                else:
+                    equation += f' {str(dictionary[key])}x**{key} {random.choice(plus_minus)}'
+    equation = equation[:-1] + '= 0'
+    print(dictionary)
+
+    return equation
+
+
+user_k = int(input("Введите натуральную степень k: "))
+print(generate_equation(user_k))
