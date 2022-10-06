422e7f3476dab900bff2a109d2d93bc0234a6ece
Ivan <vanesiotanaev@mail.ru>
2022-10-07 00:52:59 +0300

    hw4ex5 new version1

--- hw4ex5.py
+++ hw4ex5.py
@@ -41,13 +41,15 @@ def create_dictionary_keys(char_list, check_list):
     # Пройдемся по каждому элементу строки
     for item in range(0, len(char_list)):
         if char_list[item] in check_list:
-            dictionary[char_list[item]] = None
+            dictionary[int(reverse_degree(char_list[item]))] = None
     
     return dictionary
 
 def create_primary_coeff_string(char_list, check_list):
     primary_coeff_string = ''
     for i in range(len(char_list)):
+        if (i == 0 and char_list[i] == 'x'):
+            primary_coeff_string += '1'
         if (char_list[i] == 'x' and (char_list[i-1] == '+')):
             primary_coeff_string += '1'
         if (char_list[i].isdigit() == True and (not char_list[i] in check_list)) or (char_list[i] == '-'):
@@ -82,12 +84,50 @@ def create_complete_dict(dictionary, coefficients):
         
     return dictionary
 
+def sum_dicts(dict_1, dict_2):
+    general_dict = dict_1
+    for key, value in dict_2.items():
+        if key in dict_1:
+            general_dict[key] += value
+        else:
+            general_dict.update({key: value})
+    
+    return general_dict
 
+def reverse_degree(a):
+    indexes = {"\u2070": "0",
+           "\u00B9": "1",
+           "\u00B2": "2",
+           "\u00B3": "3",
+           "\u2074": "4",
+           "\u2075": "5",
+           "\u2076": "6",
+           "\u2077": "7",
+           "\u2078": "8",
+           "\u2079": "9"
+           }
+
+    degrees = ""
+    temp = str(a)
+    for char in temp:
+        degrees += indexes[char] or ""
+    
+    return degrees           
+
+def dict_sort(dict):
+
+    sorted_dict = {k: v for k, v in sorted(dict.items())}
+    items = list(sorted_dict.items())
+    sorted_dict2 = {k: v for k, v in reversed(items)}
+    print(sorted_dict2)
+    
+    return sorted_dict2
 
 char_list_1 = open_encoded_file('file4_1.txt')
 char_list_2 = open_encoded_file('file4_2.txt')
 
 special_list = create_code_values_list()
+
 dictionary_keys_1 = create_dictionary_keys(char_list_1, special_list)
 dictionary_keys_2 = create_dictionary_keys(char_list_2, special_list)
 
@@ -101,6 +141,13 @@ full_dict_1 = create_complete_dict(dictionary_keys_1, secondary_coefficients_1)
 full_dict_2 = create_complete_dict(dictionary_keys_2, secondary_coefficients_2)
 print(full_dict_1)
 print(full_dict_2)
+
+summed_dict = sum_dicts(full_dict_1, full_dict_2)
+print(summed_dict)
+
+sorted_dictionary = dict_sort(summed_dict)
+print(sorted_dictionary)
+
 
 # Конвертация
 # for item in range(0, len(char_list)):
