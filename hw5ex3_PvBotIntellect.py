85579e870320f1009f7796439f99a2eacad8c392
Ivan <vanesiotanaev@mail.ru>
2022-10-09 22:08:17 +0300

    bot_logic in progress 2

--- hw5ex3_PvBotIntellect.py
+++ hw5ex3_PvBotIntellect.py
@@ -9,7 +9,7 @@ def game():
         # Ход игрока
         while len(possible_moves) > 0:
             a = int(input('Ход игрока: '))
-            while not str(a) in possible_moves:
+            while (not str(a) in possible_moves) or dictionary[a] == 'X' or dictionary[a] == 'O':
                 a = int(input('Невозможно сделать ход на эту позицию, т.к. она занята или не существует! Ход игрока А: '))
             else:
                 possible_moves.remove(str(a))
@@ -19,7 +19,7 @@ def game():
                 break
             # Ход компьютера
             if len(possible_moves) > 0:
-                computer_move(dictionary, first_move(), count, possible_moves)
+                computer_move(possible_moves, dictionary, count)
                 graphics(dictionary)
                 if win_check(dictionary) == True:
                     break
@@ -29,14 +29,14 @@ def game():
     else:
         # Ход компьютера
         while len(possible_moves) > 0:
-            computer_move(dictionary, first_move(), count, possible_moves)
+            computer_move(possible_moves, dictionary, count)
             graphics(dictionary)
             if win_check(dictionary) == True:
                 break
             # Ход игрока
             if len(possible_moves) > 0:
                 a = int(input('Ход игрока: '))
-                while not str(a) in possible_moves:
+                while not str(a) in possible_moves or dictionary[a] == 'X' or dictionary[a] == 'O':
                     a = int(input('Невозможно сделать ход на эту позицию, т.к. она занята или не существует! Ход игрока А: '))
                 else:
                     possible_moves.remove(str(a))
@@ -47,22 +47,78 @@ def game():
         else:
             print('Ничья!')
 
-def computer_move(dict, first, iter, moves):
-    b = bot_logic(dict, first, iter, moves)
-    dict[b] = 'O'
-    print(dict[b])
-    print(dict)
-    moves.remove(str(b))
-    print(f'Ход компьютера - позиция {b}!')
+def computer_move(moves, dict, iter):
+    print()
+    if dict[5] == 'X' and iter == 0:
+        list = [1, 3, 7, 9]
+        bot_move = random.choice(list)
+    elif dict[5] != 'X' and dict[5] != 'O':
+        bot_move = 5
+    else:
+        if dict[1] == dict[4] == 'X' and (dict[7] != 'X' and dict[7] != 'O'):
+            bot_move = 7
+        elif dict[4] == dict[7] == 'X' and (dict[1] != 'X' and dict[1] != 'O'):
+            bot_move = 1
+        elif dict[2] == dict[5] == 'X' and (dict[8] != 'X' and dict[8] != 'O'):
+            bot_move = 8
+        elif dict[5] == dict[8] == 'X' and (dict[2] != 'X' and dict [2] != 'O'):
+            bot_move = 2
+        elif dict[3] == dict[6] == 'X' and (dict[9] != 'X' and dict[9] != 'O'):
+            bot_move = 9
+        elif dict[6] == dict[9] == 'X' and (dict[3] != 'X' and dict[3] != 'O'):
+            bot_move = 3
+        elif dict[1] == dict[2] == 'X' and (dict[3] != 'X' and dict[3] != 'O'):
+            bot_move = 3
+        elif dict[2] == dict[3] == 'X' and (dict[1] != 'X' and dict[1] != 'O'):
+            bot_move = 1
+        elif dict[4] == dict[5] == 'X' and (dict[6] != 'X' and dict[6] != 'O'):
+            bot_move = 6
+        elif dict[5] == dict[6] == 'X' and (dict[4] != 'X' and dict[4] != 'O'):
+            bot_move = 4
+        elif dict[7] == dict[8] == 'X' and (dict[9] != 'X' and dict[9] != 'O'):
+            bot_move = 9
+        elif dict[8] == dict[9] == 'X' and (dict[7] != 'X' and dict[7] != 'O'):
+            bot_move = 7
+        elif dict[1] == dict[5] == 'X' and (dict[9] != 'X' and dict[9] != 'O'):
+            bot_move = 9
+        elif dict[5] == dict[9] == 'X' and (dict[1] != 'X' and dict[1] != 'O'):
+            bot_move = 1
+        elif dict[3] == dict[5] == 'X' and (dict[7] != 'X' and dict[7] != 'O') :
+            bot_move = 7
+        elif dict[5] == dict[7] == 'X' and (dict[3] != 'X' and dict[3] != 'O'):
+            bot_move = 3
+        elif dict[1] == dict[3] == 'X' and (dict[2] != 'X' and dict[2] != 'O'):
+            bot_move = 2
+        elif dict[4] == dict[6] == 'X' and (dict[5] != 'X' and dict[5] != 'O'):
+            bot_move = 5
+        elif dict[7] == dict[9] == 'X' and (dict[8] != 'X' and dict[8] != 'O'):
+            bot_move = 8
+        elif dict[1] == dict[7] == 'X' and (dict[4] != 'X' and dict[4] != 'O'):
+            bot_move = 4
+        elif dict[2] == dict[8] == 'X' and (dict[5] != 'X' and dict[5] != 'O'):
+            bot_move = 5
+        elif dict[3] == dict[9] == 'X' and (dict[6] != 'X' and dict[6] != 'O'):
+            bot_move = 6
+        elif dict[1] == dict[9] == 'X' and (dict[5] != 'X' and dict[5] != 'O'):
+            bot_move = 5
+        elif dict[3] == dict[7] == 'X' and (dict[5] != 'X' and dict[5] != 'O'):
+            bot_move = 5                  
+        else:
+            choice = random.choice(moves)
+            bot_move = int(choice)
+        
+    moves.remove(str(bot_move))
+    dict[bot_move] = 'O'
+    print(f'Ход компьютера - позиция {bot_move}!')  
 
 def graphics(dict):
-    print('-------------')
+    print('=============')
     print(f'| {dict[1]} | {dict[2]} | {dict[3]} |')
     print('-------------')
     print(f'| {dict[4]} | {dict[5]} | {dict[6]} |')
     print('-------------')
     print(f'| {dict[7]} | {dict[8]} | {dict[9]} |')
-    print('-------------')
+    print('=============')
 
 def first_move():
     first = random.randint(0, 1)
@@ -79,51 +135,6 @@ def win_check(dict):
                                 or dict[3] == dict[5] == dict[7]:
                                     print('Победа!')
                                     return True
-
-def bot_logic(dict, first, iter, moves):
-    if first == 0 and dict[5] == 'X' and iter == 0:
-        bot_move = random.choice (1, 3, 7, 9)
-    elif first == 1 and dict[5] != 'X':
-        bot_move = 5
-    else:
-        if dict[1] == dict[4] == 'X' and dict[7] != 'X':
-            bot_move = 7
-        elif dict[4] == dict[7] == 'X' and dict[1] != 'X':
-            bot_move = 1
-        elif dict[2] == dict[5] == 'X' and dict[8] != 'X':
-            bot_move = 8
-        elif dict[5] == dict[8] == 'X' and dict[2] != 'X':
-            bot_move = 2
-        elif dict[3] == dict[6] == 'X' and dict[9] != 'X':
-            bot_move = 9
-        elif dict[6] == dict[9] == 'X' and dict[3] != 'X':
-            bot_move = 3
-        elif dict[1] == dict[2] == 'X' and dict[3] != 'X':
-            bot_move = 3
-        elif dict[2] == dict[3] == 'X' and dict[1] != 'X':
-            bot_move = 1
-        elif dict[4] == dict[5] == 'X' and dict[6] != 'X':
-            bot_move = 6
-        elif dict[5] == dict[6] == 'X' and dict[4] != 'X':
-            bot_move = 4
-        elif dict[7] == dict[8] == 'X' and dict[9] != 'X':
-            bot_move = 9
-        elif dict[8] == dict[9] == 'X' and dict[7] != 'X':
-            bot_move = 7
-        elif dict[1] == dict[5] == 'X' and dict[9] != 'X':
-            bot_move = 9
-        elif dict[5] == dict[9] == 'X' and dict[1] != 'X':
-            bot_move = 1
-        elif dict[3] == dict[5] == 'X' and dict[7] != 'X':
-            bot_move = 7
-        elif dict[5] == dict[7] == 'X' and dict[3] != 'X':
-            bot_move = 3              
-        else:
-            bot_move = random.choice(moves)
-    return bot_move
-
-
-
 
 game()
 
