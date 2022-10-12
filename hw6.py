766139213fbe0fdc7f4b490197deaaa03cb4b785
Ivan <vanesiotanaev@mail.ru>
2022-10-12 20:12:36 +0300

    in progress

--- hw6.py
+++ hw6.py
@@ -1,11 +1,22 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Использовал comprehension и lambda.
 
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
-
-def SumValueNumbers(value):
-    return sum(int(value[i]) for i in range(len(str(value))) if value[i].isdigit())
     
-user_value = input("Введите любое вещественное число: ")
-print(f'Сумма цифр в введенном числе равна {SumValueNumbers(user_value)}!')
+# value = input("Введите любое вещественное число: ")
+# result = lambda user_value: [int(user_value[i]) for i in range(len(str(user_value))) if user_value[i].isdigit()]
+# print(f'Сумма цифр в введенном числе равна {sum(result(value))}!')
+# print(result)
+
+# 3. Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
+# исходной последовательности.
+# Пример:
+# 47756688399943 -> [5]
+# 1113384455229 -> [8,9]
+# 1115566773322 -> []
+
+
+
+
